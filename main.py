0a8f335caf82f94837b8394892e92454acd6d087
Archana38980609 <archi.dtg@gmail.com>
2025-02-22 21:53:08 +0000

    add circleshape.py and player.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *  # Import everything from constants.py
+from player import Player
 
 # Define a background color
 BACKGROUND_COLOR = (0, 0, 0)  # Black color
@@ -7,22 +8,30 @@ BACKGROUND_COLOR = (0, 0, 0)  # Black color
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     running = True
+
     while running:
         screen.fill(BACKGROUND_COLOR)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                
+
+        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
+        player.update(dt)
+        player.draw(screen)
         pygame.display.update()
-    
+
     pygame.quit()
 
 if __name__ == "__main__":
     main()
+
