210fbc6c1d9bfdfe965b6406eaba0002cf0c63b8
Archana38980609 <archi.dtg@gmail.com>
2025-02-27 18:18:31 +0000

    feat: Add Asteroid class and integrate AsteroidField

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+
 import pygame
 from constants import *  # Import everything from constants.py
 from player import Player
-
-# Define a background color
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 BACKGROUND_COLOR = (0, 0, 0)  # Black color
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-   
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    player.containers = (updatable,drawable)
-    for sprite in drawable:
-        screen.blit(sprite.image, sprite.rect)
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     running = True
     while running:
@@ -30,12 +35,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
-        player.update(dt)
-        player.draw(screen)
+        
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.update()
-
+    
     pygame.quit()
 
 if __name__ == "__main__":
